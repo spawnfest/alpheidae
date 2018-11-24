231b780a20b0ebc926c911049960adf5d880934e
Hunter Madison <hmadison@noreply.github.com>
2018-11-23 20:22:32 -0500

    Add ranch

---
+++ lib/alpheidae/ranch_link.ex
@@ -0,0 +1,13 @@
+defmodule Alpheidae.RanchLink do
+  @moduledoc """
+  Wrapper module to get ranch into Elixir's supervisor.
+  """
+
+  @doc """
+  Starts the ranch listener, listening on the port set in the application environment.
+  """
+  def start_link do
+    options = Application.get_env(:alpheidae, :socket_options)
+    {:ok, _} = :ranch.start_listener(:alpheidae_listener, 100, :ranch_tcp, options, Alpheidae.Protocol, [])
+  end
+end
--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Alpheidae.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:exprotobuf, "~> 1.2"}
+      {:exprotobuf, "~> 1.2"},
+      {:ranch, "~> 1.4.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
