87f37ee519bff4866dd82f760d09ba16a1673d42
Hunter Madison <hmadison@noreply.github.com>
2018-11-25 10:39:15 -0500

    Make channels come from the config

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,7 @@ use Mix.Config
 config :alpheidae,
   welcome_text: "Hello!",
   max_bandwith: 32768,
+  channels: [
+    %{name: "Channel One", description: "Hello!"}
+  ],
   socket_options: [port: 5000, certfile: '_keys/cert.pem', keyfile: '_keys/key.pem']
